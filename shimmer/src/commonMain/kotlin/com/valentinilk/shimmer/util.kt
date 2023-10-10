dff3867b6676ffd6494844caff581f4d67c9c3a4
Valentin Ilk <valentin.ilk@gmail.com>
2023-10-10 15:01:00 +0200

    Fix IllegalStateException occurring on some devices

--- shimmer/src/commonMain/kotlin/com/valentinilk/shimmer/util.kt
+++ shimmer/src/commonMain/kotlin/com/valentinilk/shimmer/util.kt
@@ -2,14 +2,27 @@ package com.valentinilk.shimmer
 
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.positionInWindow
 
+/**
+ * The regular [boundsInWindow] returns a rect that is clipped by the edges of the window. So if
+ * a view is scrolled half way out of the screen, the rect returned would only have half the size
+ * required for the shimmer calculations.
+ *
+ * @return Rect that is not clipped by the window's size.
+ */
 fun LayoutCoordinates.unclippedBoundsInWindow(): Rect {
-    val positionInWindow = positionInWindow()
-    return Rect(
-        positionInWindow.x,
-        positionInWindow.y,
-        positionInWindow.x + size.width,
-        positionInWindow.y + size.height,
-    )
+    return try {
+        // Can throw an IllegalStateException on some devices
+        val positionInWindow = positionInWindow()
+        Rect(
+            positionInWindow.x,
+            positionInWindow.y,
+            positionInWindow.x + size.width,
+            positionInWindow.y + size.height,
+        )
+    } catch (_: IllegalStateException) {
+        Rect.Zero
+    }
 }
