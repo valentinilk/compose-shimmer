6c781cda04d4c230eab030e6d14ef562f8e9a369
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-03-19 00:40:03 +0300

    update deps, add wasm

---
+++ shimmer/src/skikoMain/kotlin/com/valentinilk/shimmer/ScreenInfo.skiko.kt
@@ -0,0 +1,20 @@
+package com.valentinilk.shimmer
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.toSize
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+internal actual fun rememberWindowBounds(): Rect {
+    val windowInfo = LocalWindowInfo.current.containerSize
+
+    return remember(windowInfo) {
+        Rect(Offset.Zero, windowInfo.toSize())
+    }
+}
