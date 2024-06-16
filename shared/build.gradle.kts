031b562d7a8938235a1ce54d5af9770c0782895c
v.piskunov <v.piscunov@dodopizza.com>
2024-06-16 18:41:49 +0100

    Updated libs version: - kotlin to 2.0.0 - compose to 1.6.10 Added new compose compiler

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
 }
 
 android {
