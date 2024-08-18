2f84873f135797dccae67a9792d8c599ef66986e
Valentin Ilk <valentin.ilk@gmail.com>
2024-08-18 15:11:25 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 
 plugins {
-    alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.publish).apply(false)
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.publish) apply false
     alias(libs.plugins.dokka)
     alias(libs.plugins.ktlint)
 }
